20405a482f7258a2c8ca57941c86af9d71451f0b
MURA199918 <muralisaranr18@gmail.com>
2021-01-22 11:15:24 +0530

    [Murali]Refactored to display hello from bridgelabz with a user input name

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      title: 'Welcome, hello from Bridgelabz'
+     userName: ''
     }
   }
 
@@ -19,14 +19,24 @@ class App extends React.Component {
     console.log("save button is clicked!", $event);
     window.open(this.url, "_blank");
   }
-  
+
+  onNameChange = (event) => {
+    console.log("value is ", event.target.value);
+    this.setState({ userName: event.target.value })
+  }
+
   render() {
     return (
+     <>
       <div>
-        <h1>{this.state.title}</h1>
+        <h1>Hello {this.state.userName} from Bridgelabz</h1>
         <img src={BridgeLabzLogo} onClick={this.onClick}
            alt="The Bridgelabz logo: a Bridge to Employement through lab works" />
       </div>
+      <div className="text-box">
+        <input onChange={this.onNameChange} />
+      </div>
+     </>
     );
   }
 }
